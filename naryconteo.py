cdf7b0a7a2fbbee4a95a842755d32b71b4b01da9
frank-duque <88124205+frank-duque@users.noreply.github.com>
2021-08-12 12:15:53 -0500

    Descripción dentro de función
    Cambié de lugar la descripción de la función ConAryBer

--- naryconteo.py
+++ naryconteo.py
@@ -9,15 +9,13 @@ import numpy as np
 from collections import deque   ## este deque funciona como una cola
 import matplotlib.pyplot as plt
 
-
-##Este algoritmo devuelve un vector que muestra cuantos caminos sobrevive en cada nivel
-## en un proceso beroullo
-
-# d : es la cantidad de hijos que tiene cada proceso
-# h : es la altura que tiene el arbol
-# p : es la probabilidad de percolación
-
 def ConAryBer(d,h,p):
+    ###Este algoritmo devuelve un vector que muestra cuantos caminos sobrevive en cada 
+    nivel en un proceso beroullo
+    d : es la cantidad de hijos que tiene cada proceso
+    h : es la altura que tiene el arbol
+    p : es la probabilidad de percolación
+    ###
     A=np.zeros(h)
     s=deque([[0,list(np.random.binomial(1,p,1))]])
     ## inicialmente estamos en el nivel 0 puede que percole o no
