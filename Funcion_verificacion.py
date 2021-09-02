600608f9bb329a8795e95a0083e03d1a8f3ead83
Alejandro Ramirez <dalejandro.ramirez@udea.edu.co>
2021-09-02 09:01:05 -0500

    Arreglar problema
    Se habia visto que el arbol no seguia el proceso deseado asi que se
    realizaron pruebas a mano y se cambio el C inicial de cada hijo y realizando
    la pruebas, se vio que si generaban lo que se queria

--- Funcion_verificacion.py
+++ Funcion_verificacion.py
@@ -29,46 +29,39 @@ def Funcion_verificacion(d,h):
                 self.nivel=nivel
                 self.etiqueta=np.random.uniform(0,1,1)[0]
                 self.valorC=c
-        np.random.seed(0)   ### estoy fijando el nodo semilla
+        #np.random.seed(0)   ### estoy fijando el nodo semilla
         papa=nodo(0,0)
         s=deque([papa])
-        A=[papa.etiqueta]
+        A=[round(papa.etiqueta,3)]
+        #B=[]
         Cfin=1
-
+        #for i in range(h):
+       #     B.append([np.random.uniform(0,1,1)[0] for i in range(d)])
+       # print(B)
         while len(s)!=0:
             nd=s.pop()
             hijos=[nodo(nd.nivel+1,nd.valorC) for i in range(d)]
-            for hijo in hijos:
-                A.append(hijo.etiqueta)
-            print(nd.valorC)
 #"""hasta el momento se creo todos los hijos de nd con
 #una etiqueta de 1 para asegurar su percolación, mas
 #adelante vamos a modificarla
 #"""
             for j in range(d):
-                #print(hijos[j].valorC)
-                #print(round(nd.etiqueta-hijos[j].etiqueta,3))
-
-                #print(round(nd.valorC,3))
-
+                A.append(round(hijos[j].etiqueta,3))
                 if nd.valorC<round(nd.etiqueta-hijos[j].etiqueta,3):
                     hijos[j].valorC=round(nd.etiqueta - hijos[j].etiqueta,3)
 #"""si no se puede seguir percolando con la etiqueta del papa, entonces
 #se actializa por la diferencia que se necesita """
                 elif nd.valorC>nd.etiqueta-hijos[j].etiqueta:
-                    True
-                    #hijos[j].valorC=round(hijos[j].valorC,3)
+                    hijos[j].valorC=round(hijos[j].valorC,3)
+                #print(hijos[j].nivel)
                 if hijos[j].valorC < Cfin :
                     if hijos[j].nivel==h:
                         #print(nd.valorC)
                         Cfin=min(hijos[j].valorC,Cfin)
                         continue
-                    nd=hijos[j]
-                    s.append(nd)
-
-
-            #B.append(Cfin)
-        return(A,Cfin)
+                    s.append(hijos[j])
+        return(Cfin)
     MM=Porcentaje_accesibles_nary_RMF_c(d, h)
+    
     return(MM)
-print(Funcion_verificacion(2,2))
+
--- Tabla.py
+++ Tabla.py
@@ -1,18 +1,16 @@
 import numpy as np
 import pandas as pd
 
-import rmffinal
-
+#import rmffinal
+import Funcion_verificacion as rmffinal
 
 
 
 
 def Exportar_excel_Valor_C():
-"""
-Se va a crear una lista con los valores de C
-ordenados de menos a mayor para para que
-percole
-"""
+    """Se va a crear una lista con los valores de C
+    ordenados de menos a mayor para para que
+    percole"""
 
     n= int(input("ingrese el numero de intentos que se realizará el proceso:"))
 
@@ -22,15 +20,23 @@ percole
 
     X=np.zeros(n+1)
     for i in range(1,n+1):
-        X[i]=rmffinal.Porcentaje_accesibles_nary_RMF_c(d,h)
+        X[i]=rmffinal.Funcion_verificacion(d,h)
+        #X[i]=rmffinal.Porcentaje_accesibles_nary_RMF_c(d,h)
     X=np.sort(X)
     Y=np.arange(-1/n,1,1/n)
     V={'Valores C' : X }
     df = pd.DataFrame(V, columns = ['Valores C'])
+    df.to_excel('C:/Users/digin/Dropbox/Mi PC (DESKTOP-F432S4K)/Documents/Maestria/RMF_archivosexcel/ValorC'+str(h)+'.xlsx','a',index=False,header=False)
+    
+    
+    
     #datos=pd.read_excel(r"C:\Users\digin\Dropbox\Mi PC (DESKTOP-F432S4K)\Desktop\codigos\valoresC.xlsx",header=0)
     #writer=pd.ExcelWriter('/media/daniel/00C240BEC240B9A4/Users/digin/Dropbox/Mi PC (DESKTOP-F432S4K)/Desktop/codigos/Datosexportados/ejemplo.xlsx')
-    df.to_excel('/media/daniel/00C240BEC240B9A4/Users/digin/Dropbox/Mi PC (DESKTOP-F432S4K)/Documents/Maestria/RMF_archivosexcel/ValorC'+str(h)+'.xlsx',"a+",index=False,header=False)
+    #df.to_excel('/media/daniel/00C240BEC240B9A4/Users/digin/Dropbox/Mi PC (DESKTOP-F432S4K)/Documents/Maestria/RMF_archivosexcel/ValorC'+str(h)+'.xlsx',"a+",index=False,header=False)
     #df.to_csv('/media/daniel/00C240BEC240B9A4/Users/digin/Dropbox/Mi PC (DESKTOP-F432S4K)/Documents/Maestria/RMF_archivosexcel/ValorC'+str(h)+'.csv',index=False,header=False)
-    print(df)
-    return(len(df))
+    #print(df)
+    #return(len(df))
     #df.to_excel(r"/media/daniel/00C240BEC240B9A4/Users/digin/Dropbox/Mi PC (DESKTOP-F432S4K)/Documents/Maestria/RMF_archivosexcel\valoresC300.xlsx",index=False,header=False)
+
+
+print(Exportar_excel_Valor_C())
