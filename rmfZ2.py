98d86492c63185f986db1f3b15412fc76d1f3ac7
Alejandro Ramirez <dalejandro.ramirez@udea.edu.co>
2021-10-07 11:00:19 -0500

    Actualizacion rmfz2
    Se ha modificada el rmfz2 y agregue un rmfzd sin verificar

--- rmfZ2.py
+++ rmfZ2.py
@@ -4,6 +4,7 @@ Created on Wed Sep  8 18:44:27 2021
 
 @author: digin
 """
+import random
 from collections import deque
 import numpy as np
 class nodo():
@@ -21,18 +22,21 @@ def RMF_in_Z2mas(d,h):
         nd=s.pop()
         if nd.ValorC>Cfin:
             continue
-        visitados[tuple(nd.ubicacion)]=nd.etiqueta
+        if tuple(nd.ubicacion) not in visitados:
+            visitados[tuple(nd.ubicacion)]=nd.etiqueta
         ubicacionup=[nd.ubicacion[0],nd.ubicacion[1]+1]
         ubicacionrigth=[nd.ubicacion[0]+1,nd.ubicacion[1]]
         up,rigth=tuple(ubicacionup),tuple(ubicacionrigth)
         if up in visitados:
             hijoup=nodo(ubicacionup,nd.ValorC,visitados[up])
         else :
-            hijoup=nodo(ubicacionup,nd.ValorC,np.random.uniform(0,1,1)[0])
+            visitados[up]=np.random.uniform(0,1,1)[0]
+            hijoup=nodo(ubicacionup,nd.ValorC,visitados[up])
         if rigth in visitados:
-            hijorigth=nodo(ubicacionup,nd.ValorC,visitados[rigth])
+            hijorigth=nodo(ubicacionrigth,nd.ValorC,visitados[rigth])
         else :
-            hijorigth=nodo(ubicacionup,nd.ValorC,np.random.uniform(0,1,1)[0])
+            visitados[rigth]=np.random.uniform(0,1,1)[0]
+            hijorigth=nodo(ubicacionrigth,nd.ValorC,visitados[rigth])
         if nd.ValorC<round(nd.etiqueta-hijoup.etiqueta,3):
             hijoup.ValorC=round(nd.etiqueta - hijoup.etiqueta,3)
         else:
@@ -47,12 +51,60 @@ def RMF_in_Z2mas(d,h):
                 continue
             s.append(hijoup)
             s.append(hijorigth)
+    return(len(visitados))
+    #return(Cfin)
+        
+if __name__=="__main__":
+    for i in range(30,50):
+        print(RMF_in_Z2mas(2,i))
+        #print(RMF_in_Z2mas(2,i))
+
+    
+
+    
+    
+
+
+def e(i,d):
+    ei=np.zeros(d)
+    ei[i]=1
+    return(ei)
+
+
+
+
+def RMF_in_Zdmas(d,h):
+    visitados={}
+    random.seed(0)
+    papa=nodo(np.zeros(d),0,np.random.uniform(0,1,1)[0])
+    s=deque([papa])
+    Cfin=1
+    while len(s)!=0:
+        nd=s.pop()
+        if nd.ValorC>Cfin:
+            continue
+        if tuple(nd.ubicacion) not in visitados:
+            visitados[tuple(nd.ubicacion)]=nd.etiqueta
+        for i in range(d):
+            ubicacion_i=nd.ubicacion+e(i,d)
+            if tuple(ubicacion_i) in visitados:
+                hijo_i=nodo(ubicacion_i,nd.ValorC,visitados[tuple(ubicacion_i)])
+            else:
+                visitados[tuple(ubicacion_i)]=np.random.uniform(0,1,1)[0]
+                hijo_i=nodo(ubicacion_i,nd.ValorC,visitados[tuple(ubicacion_i)])
+    
+            if nd.ValorC<round(nd.etiqueta-hijo_i.etiqueta,3):
+                hijo_i.ValorC=round(nd.etiqueta - hijo_i.etiqueta,3)
+            else:
+                hijo_i.ValorC=round(hijo_i.ValorC,3)
+            if hijo_i.ValorC<Cfin:
+                if sum(hijo_i.ubicacion)==h:
+                    Cfin=min(hijo_i.ValorC,Cfin)
+                    continue
+                s.append(hijo_i)
     return(Cfin)
         
-        
-            
-        
-print(RMF_in_Z2mas(2, 100))
+
     
     
      
