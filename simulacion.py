8939d2bfebae0237f434996e3631626c484e9810
Alejandro Ramirez <dalejandro.ramirez@udea.edu.co>
2021-11-10 09:21:28 -0500

    Modificar Simulacion
    Se separo la funcion simulacion para hacer mas facil su manipulacion
    ademas se crearon las concatenaciones de los archivos para tener todo listo

--- simulacion.py
+++ simulacion.py
@@ -12,11 +12,7 @@ import pandas as pd
 #'ValoresC1000.txt'
 
 ##   capturar nombres en un txt
-def Grafica_Acumulado(nombre_archivo,h,color):
-    '''Se realiza una grafica de ValoresC vs P(percolacion), esta probabilidad de percolacion
-       se calcula haciendo el aculumalado de las frecuencias de percolacion con cada valor de C
-    '''
-    #df = pd.read_csv('ValoresCheap/'+str(nombre_archivo))
+def Probabilidad_percolacion(nombre_archivo):
     df = pd.read_csv(str(nombre_archivo))
     df=pd.DataFrame(df)
     df=df.sort_index(axis=0, ascending=True)
@@ -32,21 +28,42 @@ def Grafica_Acumulado(nombre_archivo,h,color):
         j=j+1
     y=np.array(acumulado)/N
     unique=np.concatenate([[0],unique])
-    #y=np.concatenate([[0],y])
+    return(unique,y)
+
+def Grafica_Acumulado(nombre_archivo,h,color):
+    unique,y=Probabilidad_percolacion(nombre_archivo)
     plt.plot(unique,y,"-",label="Altura {}".format(h),color=color)
     plt.xlabel('Valor C critico')
     plt.ylabel('Probabilidad Percolacio')
     plt.title("RMF Models")
     plt.legend()
 
-    return(0)
+
+def Graficar(nombre_archivos,Etiqueta):
+    fig=plt.figure(figsize=(12,4))
+    #plt.axes([3,0.55,0.3,0.3])
+    fig.tight_layout()
+    colores=["blue","green","red"]
+    for i in range(0,3):
+        ax=plt.subplot(1,3,i+1)
+        for j in range(0,3):
+            x,y=Probabilidad_percolacion(nombre_archivos[i][j])
+            ax.plot(x,y,color=colores[j])
+        plt.legend({'Altura 500','Altura 1000','Altura 2000'})
+        plt.xlim(0,0.35)  
+        plt.ylim(0,0.2)  
+        ax.set_xlabel("Valor C")
+        ax.set_ylabel("Probabilidades")
+        ax.set_title(label=Etiqueta[i])
+    
+    plt.show()
 
 
 if __name__=='__main__':
     
-    Grafica_Acumulado('ValoresCheap/ValoresC500d2concte.txt',"500 heap","g")
-    Grafica_Acumulado('ValoresCheap/ValoresC2000d2concte.txt',"2000 heap","b")
-    Grafica_Acumulado('ValoresCheap/ValoresC1000d2concte.txt',"1000 heap d2","r")
+    #Grafica_Acumulado('ValoresCheap/ValoresC500d2concte.txt',"500 heap","g")
+    #Grafica_Acumulado('ValoresCheap/ValoresC2000d2concte.txt',"2000 heap","b")
+    #Grafica_Acumulado('ValoresCheap/ValoresC1000d2concte.txt',"1000 heap d2","r")
     #Grafica_Acumulado('ValoresCheap/ValoresC1000d3concte.txt',"1000 heap d3","r")
     #Grafica_Acumulado('ValoresC_L2/ValoresC_L2_h500concte.txt',500,"g")
     #Grafica_Acumulado('ValoresC_L2/ValoresC_L2_h1000concte.txt',1000,"r")
@@ -55,6 +72,9 @@ if __name__=='__main__':
     #Grafica_Acumulado('ValoresC_L2alt/ValoresC_L2alt_h500concte.txt',500,"b")
     #Grafica_Acumulado('ValoresC_L2alt/ValoresC_L2alt_h1000concte.txt',1000,"r")
     #Grafica_Acumulado('ValoresC_L2alt/ValoresC_L2alt_h2000concte.txt',2000,"g")
-    #Grafica_Acumulado('ValoresC_L2alt/Altura1000/ValoresC_h_1000_hora_09_30_15L2.txt',1000,"r")
-
-    plt.show()
+    Etiqueta=["2-arry","L2","L2alt"]
+    nombre_archivos=[["ValoresCheap/ValoresC500d2concte.txt","ValoresCheap/ValoresC1000d2concte.txt","ValoresCheap/ValoresC2000d2concte.txt"],
+    ["ValoresC_L2alt/ValoresC_L2alt_h500concte.txt","ValoresC_L2alt/ValoresC_L2alt_h1000concte.txt","ValoresC_L2alt/ValoresC_L2alt_h2000concte.txt"]
+    ,["ValoresC_L2/ValoresC_L2_h500concte.txt","ValoresC_L2/ValoresC_L2_h1000concte.txt","ValoresC_L2/ValoresC_L2_h2000concte.txt"]]
+    Graficar(nombre_archivos,Etiqueta)
+    #plt.show()
